8557dae14c57789edbdf8498f6cc21532913a162
Mohammed <sajidcurious9@gmail.com>
2023-02-12 04:33:17 +0530

    k

--- travel_advisory/src/components/Map/Map.jsx
+++ travel_advisory/src/components/Map/Map.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import GoogleMapReact from "google-map-react";
 
 const Map = () => {
+  const coordinates = { lat: 0, lng: 0 };
   return (
     <div className="w-[60vw]">
-      <div className="div"></div>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "w2FfyHHve6TzZogUG4zs	" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+      />
     </div>
   );
 };
