329ed28eb24ab6edf5208ec9fe813527e49fd3dc
Mohammed <sajidcurious9@gmail.com>
2023-02-12 01:54:45 +0530

    started making header and added Tailwind.css

--- travel_advisory/src/App.jsx
+++ travel_advisory/src/App.jsx
@@ -1,8 +1,16 @@
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
+
 function App() {
   return (
-    <div className="nit">
-      <div className="div">kkkkkkk</div>
-    </div>
+    <>
+      <Header />
+      <List />
+      <Map />
+      <PlaceDetails />
+    </>
   );
 }
 
---
+++ travel_advisory/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="div"></div>
+    </div>
+  );
+};
+
+export default Header;
